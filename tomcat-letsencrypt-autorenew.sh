25aad7c5daae9ef5d6143cb7fc4c1435e9bd2779
Chao Yang <chaoyang.nz@gmail.com>
2018-05-01 10:34:42 +1200

    Update tomcat-letsencrypt-autorenew.sh

--- tomcat-letsencrypt-autorenew.sh
+++ tomcat-letsencrypt-autorenew.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 set -ex
-DOMAIN=""
-TOMCAT_KEY_PASS=""
-CERTBBOT_BIN="/usr/local/bin/certbot-auto"
-EMAIL_NOTIFICATION="email_address"
+DOMAIN="my.local.stuff.co.nz"
+TOMCAT_KEY_PASS="changeit"
+CERTBBOT_BIN="/usr/local/bin/certbot"
+EMAIL_NOTIFICATION="chao.yang@stuff.co.nz"
 
 # Install certbot
 
 install_certbot () {
-    if [[ ! -f /usr/local/bin/certbot-auto ]]; then
-        wget https://dl.eff.org/certbot-auto -P /usr/local/bin
+    if [[ ! -f /usr/local/bin/certbot ]]; then
+        brew install certbot
         chmod a+x $CERTBOT_BIN
     fi
 }
@@ -23,50 +24,32 @@ renew_ssl () {
         exit 0
     else
 
-    # Create Letsencypt ssl dir if doesn't exist
-    echo "Renewing ssl certificate..."
+        # Create Letsencypt ssl dir if doesn't exist
+        echo "Renewing ssl certificate..."
 
-    # create a PKCS12 that contains both your full chain and the private key
-     rm -f /tmp/${DOMAIN}_fullchain_and_key.p12 2>/dev/null
-     openssl pkcs12 -export -out /tmp/${DOMAIN}_fullchain_and_key.p12 \
-       -passin pass:$TOMCAT_KEY_PASS \
-       -passout pass:$TOMCAT_KEY_PASS \
-       -in /etc/letsencrypt/live/$DOMAIN/fullchain.pem \
-       -inkey /etc/letsencrypt/live/$DOMAIN/privkey.pem \
-       -name tomcat
- fi
- }
+        # create a PKCS12 that contains both your full chain and the private key
+        rm -f /tmp/${DOMAIN}_fullchain_and_key.p12 2>/dev/null
+        openssl pkcs12 -export -out /tmp/${DOMAIN}_fullchain_and_key.p12 \
+            -passin pass:$TOMCAT_KEY_PASS \
+            -passout pass:$TOMCAT_KEY_PASS \
+            -in /etc/letsencrypt/live/$DOMAIN/fullchain.pem \
+            -inkey /etc/letsencrypt/live/$DOMAIN/privkey.pem \
+            -name tomcat
+    fi
+}
 
-      # Convert that PKCS12 to a JKS
+# Convert that PKCS12 to a JKS
+pkcs2jks () {
     rm -f /etc/ssl/${DOMAIN}.jks 2>/dev/null
     keytool -importkeystore -deststorepass $TOMCAT_KEY_PASS -destkeypass $TOMCAT_KEY_PASS \
       -destkeystore /etc/ssl/${DOMAIN}.jks -srckeystore /tmp/${DOMAIN}_fullchain_and_key.p12  \
       -srcstoretype PKCS12 -srcstorepass $TOMCAT_KEY_PASS \
       -alias tomcat
+}      
 
-# Send email notification on completion
-send_email_notification () {
-    if [[ $? -eq "0" ]]; then
-        echo " Retarting tomcat server"
-        systemctl restart tomcat
-        if [[ $? -eq "0" ]]; then
-            echo "" > /tmp/success
-            echo "Letsencrypt ssl certificate for $DOMAIN successfully renewed by cron job." >> /tmp/success
-            echo "" >> /tmp/success
-            echo "Tomcat successfully restarted after renewal" >> /tmp/success
-            mail -s "$DOMAIN Letsencrypt renewal" support-notify@angani.co < /tmp/success
-        else
-            echo "" > /tmp/failure
-            echo "Letsencrypt ssl certificate for $DOMAIN renewal by cron job failed." >> /tmp/failure
-            echo "" >> /tmp/failure
-            echo "Try again manually.." >> /tmp/failure
-            mail -s "$DOMAIN Letsencrypt renewal" $EMAIL_NOTIFICATION < /tmp/failure
-        fi
-    fi
-}
 
 # Main
 
 install_certbot
 renew_ssl
-send_email_notification
+pkcs2jks
